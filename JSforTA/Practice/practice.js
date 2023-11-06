e5388a7a6185a9ecae874283e7814ae19435c02c
Toma <tlaza@softserveinc.com>
2023-11-06 14:23:26 +0200

    lesson1

---
+++ JSforTA/Practice/practice.js
@@ -0,0 +1,84 @@
+//Practical Task 2
+//alert('Lazarov');
+
+//Practical Task 3
+//A)
+// let FirstName;
+// let LastName;
+
+//B)
+// FirstName = "Toma";
+// LastName = "Lazarov";
+
+//C)
+//console.log(alert(FirstName));
+//console.log(alert(LastName));
+
+//D)
+// FirstName = LastName;
+
+//E)
+// console.log(alert(FirstName));
+// console.log(alert(LastName));
+
+//Practical Task 4
+// const trainee = {
+//     firstName: "Toma",
+//     age: 37,
+//     isMarried: true,
+//     email: undefined,
+//     phone: null
+// }
+
+//Practical Task 5
+// let isAdult = confirm("Are you older than 18");
+// console.log(isAdult);
+
+//Practical Task 6
+//declare variables
+// let yourNameIs;
+// let yourLastName;
+// let studyGroup;
+// let yearOfBirth;
+
+//Assign the appropriate values to the variables
+// yourNameIs = "Toma";
+// yourLastName = "Lazarov";
+// studyGroup = "JS for TA";
+// yearOfBirth = 1987;
+
+//Create and initialize a boolean variable to store your marital status
+// let isMarried = true;
+
+//Determine the type of each variable
+// console.log(typeof(yourNameIs));
+// console.log(typeof(yourLastName));
+// console.log(typeof(studyGroup));
+// console.log(typeof(yearOfBirth));
+// console.log(typeof(isMarried));
+
+//Display the values of the variables in the console , according to their type , in the following order : Number , Boolean , String 
+// console.log(yearOfBirth);
+// console.log(isMarried);
+// console.log(yourNameIs);
+// console.log(yourLastName);
+// console.log(studyGroup);
+
+//Create 2 arbitrary variables of type Null and Undefined respectively . Output their type to the console
+// let variable1 = null;
+// let variable2 = undefined;
+// console.log(typeof(variable1));
+// console.log(typeof(variable2));
+
+//Practical Task 7
+// let userName = prompt("Tell me your Username");
+// let userEmail = prompt("What is your email");
+// let userPassword = prompt("What is your password");
+// alert(`Dear ${userName}, your email is ${userEmail}, your password is ${userPassword}.`)
+
+//Practical Task 8
+// let secsInMin = 60;
+// let secsInHour = 60 * secsInMin;
+// let secsInDay = 24 * secsInHour;
+// let secsInMonth = 31 * secsInDay;
+// alert(`There are ${secsInHour} seconds in an hour, ${secsInDay} seconds in a day and ${secsInMonth} seconds in a month of 31 days`)
